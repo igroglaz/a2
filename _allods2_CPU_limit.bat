bb8dabb30a7a151162ab3dc212a3fa3638cbead2
tangar <39904162+igroglaz@users.noreply.github.com>
2024-08-26 20:20:08 +0300

    CPU 30%

--- _allods2_CPU_limit.bat
+++ _allods2_CPU_limit.bat
@@ -3,4 +3,4 @@ rem BES don't understanding the parent directory notation `..\`
 set ALLODS_PATH=%~dp0allods2.exe
 start /B "" "%ALLODS_PATH%"
 cd .\BES_1.7.9
-start /B "" bes.exe "%ALLODS_PATH%" 15 -m
+start /B "" bes.exe "%ALLODS_PATH%" 30 -m
